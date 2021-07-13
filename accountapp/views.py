21cdb2a01aa28a89ef26990feb83ebf0c0e824ff
kimseungmun <utmdans@likelion.org>
2021-07-13 09:59:04 +0900

    7/13 3ban class commit 1

--- accountapp/views.py
+++ accountapp/views.py
@@ -2,13 +2,25 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from accountapp.models import NewModel
 
 
 def hello_world(request):
     if request.method == "POST":
+
+        temp = request.POST.get('input_text')
+
+        model_instance = NewModel()
+        model_instance.text = temp
+        model_instance.save()
+
+        data_list = NewModel.objects.all()
+
         return render(request, 'accountapp/hello_world.html',
-                      context={'text': 'POST METHOD!'})
+                      context={'data_list': data_list})
     else:
+        data_list = NewModel.objects.all()
+
         return render(request, 'accountapp/hello_world.html',
-                      context={'text': 'GET METHOD!'})
+                      context={'data_list': data_list})
 
