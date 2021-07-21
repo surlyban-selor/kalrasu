b410ac659ffa7fb5e8396c1b5a88a32c6486a849
kimseungmun <utmdans@likelion.org>
2021-07-21 16:15:36 +0900

    7/21 3ban class commit 4

--- decorators.py
+++ decorators.py
@@ -1,16 +1,63 @@
+# def decorator(func):
+#     def decorated(a, b):
+#         print('삼각형 : ')
+#         func(a, b)
+#
+#     return decorated
+#
+# @decorator
+# def triangle(a, b):
+#     while True:
+#         if a < 0 or b < 0:
+#             print('Error')
+#         else:
+#             break
+#     c = (a * b) / 2
+#     return c
+#
+#
+# triangle(3, 4)
+#
+# def decorator(func):
+#     def decorated(a, b):
+#         print('사각형 : ')
+#         func(a, b)
+#
+#     return decorated
+#
+# @decorator
+# def square(a, b):
+#     while True:
+#         if a < 0 or b < 0:
+#             print('Error')
+#         else :
+#             break
+#     c = a * b
+#     return c
+#
+#
+# square(3, 4)
 
 
-def decorator(func):
-    def decorated(input_text):
-        print('함수 시작!')
-        func(input_text)
-        print('함수 끝!')
-
+def check_integer(func):
+    def decorated(width, height):
+        if width >= 0 and height >= 0:
+            return func(width, height)
+        else:
+            raise ValueError('input positive int')
     return decorated
 
-@decorator
-def hello_world(input_text):
-    print(input_text)
+
+@check_integer
+def square(w, h):
+    b = w * h
+    return b
+
+@check_integer
+def triangle(w, h):
+    c = (w * h) / 2
+    return c
 
 
-hello_world('Hello World')
+square(5, 6)
+triangle(8, 5)
