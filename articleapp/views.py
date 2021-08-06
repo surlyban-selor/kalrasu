10c289444782819bc4cf6bd61182febd9742294f
kimseungmun <utmdans@likelion.org>
2021-08-06 10:01:25 +0900

    8/6 3ban class commit 2

--- articleapp/views.py
+++ articleapp/views.py
@@ -1,10 +1,12 @@
+
+
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse_lazy, reverse
 from django.utils.decorators import method_decorator
-from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
+from django.views.generic import CreateView, DetailView, UpdateView, DeleteView, ListView
 
 from articleapp.decorators import article_ownership_required
 from articleapp.forms import ArticleCreationForm
@@ -49,3 +51,13 @@ class ArticleDeleteView(DeleteView):
     context_object_name = 'target_article'
     success_url = reverse_lazy('article:list')
     template_name = 'articleapp/delete.html'
+
+
+
+class ArticleListView(ListView):
+    model = Article
+    context_object_name = 'article_list'
+    template_name = 'articleapp/list.html'
+    paginate_by = 20
+    # 리스트 뷰 로직
+
