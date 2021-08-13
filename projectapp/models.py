d16476eb0298305d95815ea124df62c3e344171d
kimseungmun <utmdans@likelion.org>
2021-08-13 10:26:21 +0900

    8/13 3ban class commit 5(1)

--- projectapp/models.py
+++ projectapp/models.py
@@ -6,6 +6,6 @@ from django.db import models
 class Project(models.Model):
     name = models.CharField(max_length=20, null=False)
     description = models.CharField(max_length=300, null=True)
-    image = models.ImageField(upload_to='project/', )
+    image = models.ImageField(upload_to='project/', null=False)
 
     create_at = models.DateTimeField(auto_now_add=True)
