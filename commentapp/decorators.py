639a9c437e75e1a11c70d2e7c88b43770d42cf53
kimseungmun <utmdans@likelion.org>
2021-08-16 23:21:20 +0900

    8/16 3ban class commit (1)

--- commentapp/decorators.py
+++ commentapp/decorators.py
@@ -5,7 +5,7 @@ from commentapp.models import Comment
 
 def comment_ownership_required(func):
     def decorated(request, *args, **kwargs):
-        target_comment = Comment.objects.get(pk=kwargs)
+        target_comment = Comment.objects.get(pk=kwargs['pk'])
         if target_comment.writer == request.user:
             return func(request, *args, **kwargs)
         else:
